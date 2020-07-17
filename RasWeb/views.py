c854f765f7168c77b3da1e0587dba03f9f168410
Anh Viet Vo <anhviet5121pc@gmail.com>
2020-07-17 18:30:20 +0700

    Update foundation

--- RasWeb/views.py
+++ RasWeb/views.py
@@ -1,14 +1,14 @@
 from django.shortcuts import render
 import time
-import RPi.GPIO as GPIO 
+# import RPi.GPIO as GPIO 
 
 # Create your views here.
 def index(request):
     return render(request, "RasWeb/index.html")
 
-LED_PIN = 18
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(LED_PIN, GPIO.OUT)
+# LED_PIN = 18
+# GPIO.setmode(GPIO.BOARD)
+# GPIO.setup(LED_PIN, GPIO.OUT)
 
 def turn_on(request):
     GPIO.output(LED_PIN, GPIO.HIGH)
@@ -28,5 +28,5 @@ def blink(request):
         time.sleep(1)
         GPIO.output(LED_PIN, GPIO.LOW)
         time.sleep(1)
-    return render(request, "RasWeb/index.html")
+    return render(request, "index.html")
  
