42fcff0c3d873a00dff675552f251385f5bf8985
Anh Viet Vo <anhviet5121pc@gmail.com>
2020-07-18 22:53:18 +0700

    Delete modal and update single page

--- RasWeb/urls.py
+++ RasWeb/urls.py
@@ -4,7 +4,5 @@ from . import views
 app_name = 'light_control'
 urlpatterns = [
     path('', views.index, name='index'),
-    path('on', views.turn_on, name='turn_on'),
-    path('off', views.turn_off, name='turn_off'),
-    path('blink', views.blink, name='blink')
+    path('status/<str:stt>', views.status, name='status')
 ]
--- RasWeb/views.py
+++ RasWeb/views.py
@@ -1,32 +1,29 @@
 from django.shortcuts import render
+from django.http import HttpResponse, Http404
 import time
-# import RPi.GPIO as GPIO 
+import RPi.GPIO as GPIO 
 
 # Create your views here.
 def index(request):
     return render(request, "RasWeb/index.html")
 
-# LED_PIN = 18
-# GPIO.setmode(GPIO.BOARD)
-# GPIO.setup(LED_PIN, GPIO.OUT)
+LED_PIN = 18
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(LED_PIN, GPIO.OUT)
 
-def turn_on(request):
-    GPIO.output(LED_PIN, GPIO.HIGH)
-    return render(request, "RasWeb/index.html", {
-        "status": "on"
-    })
-
-def turn_off(request):
-    GPIO.output(LED_PIN, GPIO.LOW)
-    return render(request, "RasWeb/index.html", {
-        "status": "off"
-    })
-
-def blink(request):
-    for i in range(10):
+def status(request, stt):   
+    if stt == 'on':
         GPIO.output(LED_PIN, GPIO.HIGH)
-        time.sleep(1)
+        return HttpResponse()     
+    elif stt == 'off':
         GPIO.output(LED_PIN, GPIO.LOW)
-        time.sleep(1)
-    return render(request, "index.html")
- 
+        return HttpResponse()
+    elif stt == 'blink':
+        for i in range(10):
+            GPIO.output(LED_PIN, GPIO.HIGH)
+            time.sleep(0.5)
+            GPIO.output(LED_PIN, GPIO.LOW)
+            time.sleep(0.5)
+        return HttpResponse()
+    else:
+        return Http404('No such status')
